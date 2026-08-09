02ae5c021ff4c5c688b9792084f1fe9e29d30955
ea-arnob-07 <eaarnob178@gmail.com>
2026-08-09 20:30:24 +0600

    Use modern Vercel API routing structure

---
+++ api/index.py
@@ -0,0 +1,12 @@
+import os
+import sys
+
+# Add project root to sys.path
+root_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if root_path not in sys.path:
+    sys.path.insert(0, root_path)
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
+from django.core.wsgi import get_wsgi_application
+app = get_wsgi_application()
