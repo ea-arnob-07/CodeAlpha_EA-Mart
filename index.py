4cf56a158007f601e598b2449154ae542021fb2d
ea-arnob-07 <eaarnob178@gmail.com>
2026-08-09 20:23:51 +0600

    Add error catching to index.py to expose Vercel startup errors

--- index.py
+++ index.py
@@ -6,7 +6,14 @@ path = os.path.dirname(os.path.abspath(__file__))
 if path not in sys.path:
     sys.path.append(path)
 
-from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
-app = get_wsgi_application()
+
+try:
+    from django.core.wsgi import get_wsgi_application
+    app = get_wsgi_application()
+except Exception as e:
+    import traceback
+    err = traceback.format_exc()
+    def app(environ, start_response):
+        start_response('500 Internal Server Error', [('Content-Type', 'text/plain')])
+        return [err.encode('utf-8')]
