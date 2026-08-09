c0fea5c19d5415f791b758ed88800db5197a0d9a
ea-arnob-07 <eaarnob178@gmail.com>
2026-08-09 20:50:04 +0600

    Fix Vercel 404 routing and push db with superuser

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,4 +1,10 @@
 import os
+import sys
+
+# Add project root to sys.path
+root_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if root_path not in sys.path:
+    sys.path.insert(0, root_path)
 
 from django.core.wsgi import get_wsgi_application
 
